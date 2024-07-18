3a5c1f923dfd090eb021c29a4d9582f6c5b98867
Mahipal Singh <mahipalsinghchauhan1995@gmail.com>
2024-07-18 15:56:13 +0530

    codes

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+from fwp.exception import XRayException
+from fwp.pipeline.training_pipeline import TrainPipeline
+
+
+def start_training():
+    try:
+        train_pipeline = TrainPipeline()
+
+        train_pipeline.run_pipeline()
+
+    except Exception as e:
+        raise fwpException(e, sys)
+
+
+if __name__ == "__main__":
+    start_training()
