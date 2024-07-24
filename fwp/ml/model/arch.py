47a9cd6913c8ba7aee3c14e76a479a21fade371f
Mahipal singh <140134175+Mahipalsingh6695@users.noreply.github.com>
2024-07-24 13:45:56 +0530

    Update arch.py
    vgg 16 update

--- fwp/ml/model/arch.py
+++ fwp/ml/model/arch.py
@@ -1,157 +1,21 @@
+import torch
 import torch.nn as nn
+import torchvision.models as models
 import torch.nn.functional as F
-
 
 class Net(nn.Module):
     def __init__(self):
         """
-        Creating custom CNN architecture for Image classification
+        Replacing the custom CNN architecture with VGG16 for Image classification
         """
         super(Net, self).__init__()
+        
+        # Load VGG16 model with pretrained weights
+        self.model = models.vgg16(pretrained=True)
+        
+        # Replace the final fully connected layer to match the number of classes in your dataset
+        num_features = self.model.classifier[6].in_features
+        self.model.classifier[6] = nn.Linear(num_features, 2)  # Assuming 2 classes: Original and Fake
 
-        self.convolution_block1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=3, out_channels=8, kernel_size=(3, 3), padding=0, bias=True
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(8),
-        )
-
-        self.pooling11 = nn.MaxPool2d(2, 2)
-
-        self.convolution_block2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8, out_channels=20, kernel_size=(3, 3), padding=0, bias=True
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(20),
-        )
-
-        self.pooling22 = nn.MaxPool2d(2, 2)
-
-        self.convolution_block3 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=20,
-                out_channels=10,
-                kernel_size=(1, 1),
-                padding=0,
-                bias=True,
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(10),
-        )
-
-        self.pooling33 = nn.MaxPool2d(2, 2)
-
-        self.convolution_block4 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=10,
-                out_channels=20,
-                kernel_size=(3, 3),
-                padding=0,
-                bias=True,
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(20),
-        )
-
-        self.convolution_block5 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=20,
-                out_channels=32,
-                kernel_size=(1, 1),
-                padding=0,
-                bias=True,
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-        )
-
-        self.convolution_block6 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=32,
-                out_channels=10,
-                kernel_size=(3, 3),
-                padding=0,
-                bias=True,
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(10),
-        )
-
-        self.convolution_block7 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=10,
-                out_channels=10,
-                kernel_size=(1, 1),
-                padding=0,
-                bias=True,
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(10),
-        )
-
-        self.convolution_block8 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=10,
-                out_channels=14,
-                kernel_size=(3, 3),
-                padding=0,
-                bias=True,
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(14),
-        )
-
-        self.convolution_block9 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=14,
-                out_channels=16,
-                kernel_size=(3, 3),
-                padding=0,
-                bias=True,
-            ),
-            nn.ReLU(),
-            nn.BatchNorm2d(16),
-        )
-
-        self.gap = nn.Sequential(nn.AvgPool2d(kernel_size=4))
-
-        self.convolution_block_out = nn.Sequential(
-            nn.Conv2d(
-                in_channels=16, out_channels=2, kernel_size=(4, 4), padding=0, bias=True
-            ),
-        )
-
-    def forward(self, x) -> float:
-        x = self.convolution_block1(x)
-
-        x = self.pooling11(x)
-
-        x = self.convolution_block2(x)
-
-        x = self.pooling22(x)
-
-        x = self.convolution_block3(x)
-
-        x = self.pooling33(x)
-
-        x = self.convolution_block4(x)
-
-        x = self.convolution_block5(x)
-
-        x = self.convolution_block6(x)
-
-        x = self.convolution_block7(x)
-
-        x = self.convolution_block8(x)
-
-        x = self.convolution_block9(x)
-
-        x = self.gap(x)
-
-        x = self.convolution_block_out(x)
-
-        x = x.view(-1, 2)
-
-        return F.log_softmax(x, dim=-1)
+    def forward(self, x):
+        return self.model(x)
